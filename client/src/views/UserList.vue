47dadec9ea259a32e24bf24ecf4255260777d9ad
Pedro Diaz <pedro.diaz.vargas@gmail.com>
2020-11-06 21:55:46 -0500

    integrating with API

--- client/src/views/UserList.vue
+++ client/src/views/UserList.vue
@@ -7,7 +7,7 @@
 <script>
 import UserCard from '@/components/UserCard.vue'
 import SampleUsers from '@/sample/users'
-// import axios from 'axios'
+import axios from 'axios'
 
 export default {
   components: {
@@ -15,19 +15,19 @@ export default {
   },
   data() {
     return {
-      users: SampleUsers.users
+      users: []
     }
   },
-  // created() {
-  //   axios
-  //     .get('http://localhost:3000/events')  // Does a get request
-  //     .then(response => {
-  //       console.log(response.data) // For now, logs out the response
-  //     })
-  //     .catch(error => {
-  //       console.log('There was an error:', error.response) // Logs out the error
-  //     })
-  // }
+  created() {
+    axios
+      .get('https://wkohg0bgy8.execute-api.us-east-2.amazonaws.com/Prod/users')  // Does a get request
+      .then(response => {
+        this.users = response.data;
+      })
+      .catch(error => {
+        console.log('There was an error:', error.response) // Logs out the error
+      })
+  }
 }
 </script>
 
