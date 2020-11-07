d7ffe93af042652bcf789c2291525181c0d0d67d
Pedro Diaz <pedro.diaz.vargas@gmail.com>
2020-11-06 20:54:19 -0500

    adding comments

--- server/api/dynamo.py
+++ server/api/dynamo.py
@@ -10,7 +10,19 @@ import time
 _users_table = boto3.resource("dynamodb").Table("slackatto-users")
 
 
+def get_users():
+    """
+    Read all the users from the dynamo database.
+    """
+    response = _users_table.scan(Select="ALL_ATTRIBUTES")
+    return [json.loads(record["user_obj"]) for record in response.get("Items", [])]
+
+
 def add_or_update_user(user):
+    """
+    Given the information of a user as a json object, add the user to
+    the dynamo database. The identifier of ther user will be it's unique id.
+    """
     return _users_table.update_item(
         Key={"id": user["id"]},
         UpdateExpression="SET user_obj = :user_obj, username = :username",
@@ -22,5 +34,8 @@ def add_or_update_user(user):
 
 
 def remove_user(user):
+    """
+    Delete a user from the dynamo database.
+    """
     return _users_table.delete_item(Key={"id": user["id"]})
 
--- server/api/lambda_handler.py
+++ server/api/lambda_handler.py
@@ -10,7 +10,12 @@ app = Minik()
 
 
 @app.post("/events")
-def post_event():
+def slack_event_handler():
+    """
+    Event handler responsible for taking care of the requests sent by slack.
+    Different type of events include verification and user changes. User changes
+    are persisted in a dynamo database.
+    """
     payload = app.request.json_body
     request_type = payload.get("type")
 
@@ -23,15 +28,36 @@ def post_event():
     return event_handlers_by_type.get(request_type, no_op)(payload)
 
 
+@app.get("/users")
+def get_users():
+    """
+    Endpoint used to retrieve the set of users in the dynamo database.
+    """
+    return dynamo.get_users()
+
+
 def no_op(payload):
+    """
+    The base case in which the given request type is not valid.
+    """
     return {"error": "Invalid request type received."}
 
 
 def return_challenge(payload):
+    """
+    If the type of event is the slack challenge, return the value sent
+    by their API. This response will complete the validation.
+    """
     return payload["challenge"]
 
 
 def user_change_handler(payload):
+    """
+    This handler will take care of managing user events. A user event
+    can be defined a change in the profile of a given user, an addition
+    of the user to the slack workspace or the removal/deactivate of a
+    user from the workspace.
+    """
     user = payload["event"]["user"]
 
     if user.get("deleted", False):
@@ -40,6 +66,11 @@ def user_change_handler(payload):
         dynamo.add_or_update_user(user)
 
 
+# Keep track of the type of events supported and their respective handler.
+# This approach allows us to update the event type, while keeping the same
+# implementation. I.e. say that slack no longer sends a `url_verification`,
+# but a `url_validation`. In this case, we would only need to update this
+# mapping.
 event_handlers_by_type = {
     "url_verification": return_challenge,
     "user_change": user_change_handler,
